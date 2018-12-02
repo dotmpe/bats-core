eabe63109529efd04253772ea22cfebfcc09aabd
B. van Berkum <dev@dotmpe.com>
2018-12-02 03:57:15 +0100

    Improved install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ set -e
 BATS_ROOT="${0%/*}"
 PREFIX="$1"
 
+sed -i.bak 's/^export\ BATS_VERSION=.*/export BATS_VERSION='"$(git describe|cut -c2-)"'/g' "libexec/bats-core/bats"
+rm "libexec/bats-core/bats.bak"
+
 if [[ -z "$PREFIX" ]]; then
   printf '%s\n' \
     "usage: $0 <prefix>" \
@@ -18,4 +21,5 @@ install -m 755 "$BATS_ROOT/libexec/bats-core"/* "$PREFIX/libexec/bats-core"
 install -m 644 "$BATS_ROOT/man/bats.1" "$PREFIX/share/man/man1"
 install -m 644 "$BATS_ROOT/man/bats.7" "$PREFIX/share/man/man7"
 
+git checkout "libexec/bats-core/bats"
 echo "Installed Bats to $PREFIX/bin/bats"
